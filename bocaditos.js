04eaa21640a246bbeeefa3aa11f3de625f8bfc24
Juan David Duarte <juandis981@gmail.com>
2023-07-03 13:34:30 -0500

    agregue la secion de iniciar secion

--- bocaditos.js
+++ bocaditos.js
@@ -1,6 +1,9 @@
 let botonHamburguesaIni = document.getElementById('botonHamburguesa')
 let botonPicadaIni = document.getElementById('botonPicada')
 let botonArepaIni = document.getElementById('botonArepa')
+let botonDeiniciarSecion = document.getElementById('bot-ini')
+
+let sectionPrin = document.getElementById('tarjetas_comidas')
 
 let sectionHambur = document.getElementById('catHamburguesas')
 sectionHambur.style.display = 'none'
@@ -8,35 +11,33 @@ let sectionPicada = document.getElementById('catPicadas')
 sectionPicada.style.display = 'none'
 let sectionArepa = document.getElementById('catArepas')
 sectionArepa.style.display = 'none'
-
-
-botonHamburguesaIni.addEventListener('click', catalogoPrin )
-botonPicadaIni.addEventListener('click', catalogoPrin )
-botonArepaIni.addEventListener('click', catalogoPrin )
-
+let sectionSecion = document.getElementById('secSecion')
+sectionSecion.style.display = 'none'
 
 botonHamburguesaIni.addEventListener('click', mostrarHambur )
 botonPicadaIni.addEventListener('click', mostrarPicada )
 botonArepaIni.addEventListener('click', mostrarArepa )
+botonDeiniciarSecion.addEventListener('click', initSection)
 
-
-
-function catalogoPrin () {
-    let sectionPrin = document.getElementById('tarjetas_comidas')
+function initSection () {
+    botonDeiniciarSecion.style.display = 'none'
     sectionPrin.style.display = 'none'
+    sectionSecion.style.display = 'block'
 }
-
 function mostrarHambur () {
-    let sectionHambur = document.getElementById('catHamburguesas')
+    botonDeiniciarSecion.style.display = 'none'
+    sectionPrin.style.display = 'none'
     sectionHambur.style.display = 'block'
 }
 function mostrarPicada () {
-    let section = document.getElementById('catPicadas')
-    section.style.display = 'block'
+    botonDeiniciarSecion.style.display = 'none'
+    sectionPrin.style.display = 'none'
+    sectionPicada.style.display = 'block'
 }
 function mostrarArepa () {
-    let section = document.getElementById('catArepas')
-    section.style.display = 'block'
+    botonDeiniciarSecion.style.display = 'none'
+    sectionPrin.style.display = 'none'
+    sectionArepa.style.display = 'block'
 }
 
 
