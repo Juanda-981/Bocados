1f047f7b467a788586eb8e372a462610f57aa4d8
Juan David Duarte <juandis981@gmail.com>
2023-07-04 17:54:37 -0500

    version estable antes de agregar patacones y salchipapas

--- bocaditos.js
+++ bocaditos.js
@@ -3,6 +3,12 @@ let botonPicadaIni = document.getElementById('botonPicada')
 let botonArepaIni = document.getElementById('botonArepa')
 let botonDeiniciarSecion = document.getElementById('bot-ini')
 
+let extCatHambur = document.getElementById('salir-de-hambur')
+let extCatPicada = document.getElementById('salir-de-picadas')
+let extCatArepa = document.getElementById('salir-de-arepas')
+let extInitSection = document.getElementById('salir-de-initSection')
+let extVerifiedcuenta = document.getElementById('salir-de-pedidos')
+
 let sectionPrin = document.getElementById('tarjetas_comidas')
 
 let sectionHambur = document.getElementById('catHamburguesas')
@@ -13,11 +19,46 @@ let sectionArepa = document.getElementById('catArepas')
 sectionArepa.style.display = 'none'
 let sectionSecion = document.getElementById('secSecion')
 sectionSecion.style.display = 'none'
+let sectionPedidos = document.getElementById('seccion-de-pedidos')
+sectionPedidos.style.display = 'none'
+
+extCatHambur.addEventListener('click',salirDeHamburguesas )
+extCatPicada.addEventListener('click',salirDePicada )
+extCatArepa.addEventListener('click',salirDeArepa )
+extInitSection.addEventListener('click', salirDeIniSesion )
+extVerifiedcuenta.addEventListener('click', salirDePedidos )
 
 botonHamburguesaIni.addEventListener('click', mostrarHambur )
 botonPicadaIni.addEventListener('click', mostrarPicada )
 botonArepaIni.addEventListener('click', mostrarArepa )
 botonDeiniciarSecion.addEventListener('click', initSection)
+
+function salirDeHamburguesas () {
+    sectionHambur.style.display = 'none'
+    sectionPrin.style.display = 'block'
+    botonDeiniciarSecion.style.display = 'block'
+}
+function salirDePicada () {
+    sectionPicada.style.display = 'none'
+    sectionPrin.style.display = 'block'
+    botonDeiniciarSecion.style.display = 'block'
+}
+function salirDeArepa () {
+    sectionArepa.style.display = 'none'
+    sectionPrin.style.display = 'block'
+    botonDeiniciarSecion.style.display = 'block'
+}
+function salirDeIniSesion () {
+    sectionSecion.style.display = 'none'
+    sectionPrin.style.display = 'block'
+    botonDeiniciarSecion.style.display = 'block'
+}
+function salirDePedidos () {
+    sectionPedidos.style.display = 'none'
+    sectionPrin.style.display = 'block'
+    botonDeiniciarSecion.style.display = 'block'
+}
+
 
 function initSection () {
     botonDeiniciarSecion.style.display = 'none'
@@ -40,12 +81,13 @@ function mostrarArepa () {
     sectionArepa.style.display = 'block'
 }
 
-function ir(){
+function contraVerifi(){
  let contraseña="Bocados-Nemococo"
  let user="Dilan-Bocados.com"
 
  if(document.form.password.value==contraseña && document.form.login.value==user){
     sectionSecion.style.display = 'none'
+    sectionPedidos.style.display = 'block'
  }
  else{
     alert("por favor ingresa nombre y usuario correctos")
